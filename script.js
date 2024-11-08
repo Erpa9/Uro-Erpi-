f4e24fb47790eb108dd0401041cdcf9934b018f8
Erpa9 <erpicew@gmail.com>
2024-11-08 20:11:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Display a welcome message when the page loads
+window.onload = function() {
+    alert("Welcome to My Webpage!");
+};
+/* Active tab styling */
+nav ul li a.active {
+    color: yellow; /* Change this to any color you like */
+    font-weight: bold;
+}
+    // Remove the active class from all links and add to the clicked link
+    tabLinks.forEach(link => link.classList.remove("active"));
+    this.classList.add("active");
